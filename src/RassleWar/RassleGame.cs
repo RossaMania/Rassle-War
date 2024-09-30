d9b5ad4b6d7f4dba46094e40b7815fe3e75f7ef5
RossaMania <raclettenberg@gmail.com>
2024-09-30 17:16:26 -0500

    Refactor RasslingMatch into two new methods. Replaced RasslingMatch with BothStandingNeutral and BothStandingGrappling methods. BothStandingNeutral offers options to lock up, slap, or taunt the opponent, leading to either BothStandingGrappling or back to itself. BothStandingGrappling offers options to push, try to take down, or break the hold, looping back to BothStandingNeutral. Updated StartGame to call BothStandingNeutral.

--- src/RassleWar/RassleGame.cs
+++ src/RassleWar/RassleGame.cs
@@ -22,9 +22,61 @@ namespace RassleWar
             }
         }
 
-        public void RasslingMatch()
+        public void BothStandingNeutral()
         {
             Console.WriteLine("And the unstoppable force faces the immovable object!");
+            Console.WriteLine("What is the player going to do?");
+            Console.WriteLine("1. Lock up with opponent!");
+            Console.WriteLine("2. Slap opponent!");
+            Console.WriteLine("3. Taunt opponent!");
+
+            var playerMove = Console.ReadLine() ?? string.Empty;
+            int playerChoice = int.Parse(playerMove);
+
+            if (playerChoice == 1)
+            {
+                Console.WriteLine("The two wrestlers lock up in the center of the ring!");
+                BothStandingGrappling();
+            }
+            else if (playerChoice == 2)
+            {
+                Console.WriteLine("The player slaps the opponent across the face!");
+                BothStandingNeutral();
+            }
+            else if (playerChoice == 3)
+            {
+                Console.WriteLine("The player taunts the opponent!");
+                BothStandingNeutral();
+            }
+        }
+
+        public void BothStandingGrappling()
+        {
+            Console.WriteLine("The two wrestlers are locked up in the center of the ring!");
+            Console.WriteLine("What is the player going to do?");
+            Console.WriteLine("1. Push opponent into the ropes!");
+            Console.WriteLine("2. Try to take opponent down!");
+            Console.WriteLine("3. Break the hold!");
+
+            var playerMove = Console.ReadLine() ?? string.Empty;
+            int playerChoice = int.Parse(playerMove);
+
+            if (playerChoice == 1)
+            {
+                Console.WriteLine("The player pushes the opponent into the ropes!");
+                BothStandingNeutral();
+            }
+            else if (playerChoice == 2)
+            {
+                Console.WriteLine("The player tries to take the opponent down!");
+                BothStandingNeutral();
+            }
+            else if (playerChoice == 3)
+            {
+                Console.WriteLine("The player breaks the hold!");
+                BothStandingNeutral();
+            }
+
         }
 
         public void StartGame()
@@ -39,7 +91,7 @@ namespace RassleWar
 
                 if (string.Equals(response, "Y", StringComparison.OrdinalIgnoreCase))
                 {
-                    RasslingMatch();
+                    BothStandingNeutral();
                 }
 
                 if (string.Equals(response, "N", StringComparison.OrdinalIgnoreCase))
